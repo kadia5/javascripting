7a4d716c2ac709d57f74e0f0a33bdbce99f429a0
kadia5 <kdabraeux@gmail.com>
2021-06-14 19:31:21 +0000

    whichschools assignmt.

---
+++ conditionals-whichschool.js
@@ -0,0 +1,11 @@
+const whichSchool  = function (age) {
+  if (age <=12){
+  return "Elementary School";
+}
+else if(age >=13 && age <=18){
+  return "Secondary School";
+}
+else {
+  return "Lighthouse Labs";
+}
+};
